b8be2d18e73a8562d38fecf0e8dda3cc01f59201
Devon S <111747820+DevonSav@users.noreply.github.com>
2022-10-24 11:15:12 +0200

    Update temperature.js

--- temperature.js
+++ temperature.js
@@ -11,33 +11,38 @@ while (!validInputType)
 	inputTempType = prompt(`In which metric is the temperature you are converting?
 C - Celsius
 F - Fahrenheit
-K - Kelvin`).toLowerCase(); // By converting the input to lowercase we can accept both lower and uppercase characters
+K - Kelvin`).toLowerCase(); // Using lowercase we can accept both lower and uppercase characters
 
 	switch (inputTempType) {
-		case "c":case "f":case "k":	// Only if the input matches one of these is it valid
+		// Only if the input matches one of these is it valid
+		case "c":case "f":case "k":
 			validInputType = true;
 			break;
 
 		default:
-			console.log("ERROR: Input type is invalid!");	// Otherwise just tell the user it's invalid and ask again
+			// Tell the user it's invalid and ask again
+			console.log("ERROR: Input type is invalid!");
 			break;
 	}
 }
 
-// Ask for input temperature, check that it is a number and only continue when valid using a loop
+// Ask for input temperature, check that it is a number and only continue when valid
 let validTemp = false
 while (!validTemp)
 {
 	inputTemperature = prompt("Input a temperature value: ");
-	if (Number.isInteger(Math.round(Number(inputTemperature)))){	// Input is rounded(only during this check) to be able to use '.isInteger' on it as other number checking methods seem a bit messy
+
+	// Input is rounded(only during this check) to be able to use '.isInteger' on it as other number checking methods seem a bit messy
+	if (Number.isInteger(Math.round(Number(inputTemperature)))){
 		validTemp = true;
+		break;
 	}
 	else{
 		console.log("ERROR: Input is invalid!");
 	}
 }
 
-// Ask for output metric and only continue when valid using a loop
+// Ask for output metric and only continue when valid
 let validOutputType = false
 while (!validOutputType)
 {
@@ -47,7 +52,8 @@ F - Fahrenheit
 K - Kelvin`).toLowerCase();
 
 	switch (outputTempType) {
-		case "c":case "f":case "k":	// Like before; Only if the input matches one of these is it valid
+		// Like before; Only if the input matches one of these is it valid
+		case "c":case "f":case "k":
 			validOutputType = true;
 			break;
 
@@ -61,11 +67,18 @@ K - Kelvin`).toLowerCase();
 // Combine input and output metric characters to form a "conversion code" for use below
 let tempCode = inputTempType + outputTempType
 
-// Note: the output temperature is rounded to look nice, the input is not(so the user knows that their input was taken/calculated accurately). This might be unnecessary but seemed better for clarity
+/* Note: the output temperature is rounded to look nice, however the input is not.
+ * This is so the user knows that their input was taken/calculated accurately).
+ * This might be unnecessary but seemed better for clarity
+ */
 switch (tempCode){
 	case "cf":  // Celsius to Fahrenheit
-		outputTemperature = Number(inputTemperature) * 9/5 + 32;	// Apply relevant calculations (this is done the same for all cases below just with different math)
-		console.log(inputTemperature + "\u00B0C is " + outputTemperature.toFixed(3) + "\u00B0F.")	// Return the values in a nice log message	| Note: '\u00B0' is a special code for the degrees symbol; directly pasting means it doesn't display correctly
+		// Apply relevant calculations (this is done the same for all cases below just with different math)
+		outputTemperature = Number(inputTemperature) * 9/5 + 32;
+		/* Returns the values in a nice log message.
+		 * Note: '\u00B0' is the code for the degrees symbol; using the symbol directly means it doesn't display correctly.
+		 */
+		console.log(inputTemperature + "\u00B0C is " + outputTemperature.toFixed(3) + "\u00B0F.")
 		break;
 
 	case "ck":  // Celsius to Kelvin
@@ -93,14 +106,14 @@ switch (tempCode){
 		console.log(inputTemperature + "K is " + outputTemperature.toFixed(3) + "\u00B0F.")
 		break;
 
-	default:
+	default:  // Should this somehow be reached print an error message
 		console.log("ERROR: Something went terribly wrong when converting!");
 		break;
 }
 
 
 /* REFERENCES
-Use of '\u00B0' to correctly display degree symbol: http://gdichicago.com/courses/gdi-featured-js-intro/homework.html#:~:text=Unicode%20Characters%3A%20To%20print%20the,character%20for%20the%20degress%20symbol.
-For rounding values: https://www.w3schools.com/js/js_number_methods.asp
-How to shorten switch statements: https://www.codegrepper.com/code-examples/javascript/javascript+switch+statement+multiple+cases
-*/
+ * 1. Use of '\u00B0' to correctly display degree symbol: http://gdichicago.com/courses/gdi-featured-js-intro/homework.html#:~:text=Unicode%20Characters%3A%20To%20print%20the,character%20for%20the%20degress%20symbol.
+ * 2. For rounding values: https://www.w3schools.com/js/js_number_methods.asp
+ * 3. How to shorten switch statements: https://www.codegrepper.com/code-examples/javascript/javascript+switch+statement+multiple+cases
+ */
